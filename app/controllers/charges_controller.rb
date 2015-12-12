323628ecdac3a70d7509bd5db585d7da0db9126b
Simone <Simone.v.gill@wpfentongroup.co.uk>
2015-12-12 11:12:54 +0530

    final form adjustments complete day 13

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -11,7 +11,7 @@ class ChargesController < ApplicationController
 
 						  charge = Stripe::Charge.create(
 						    :customer    => customer.id,
-						    :amount      => @amount,
+						    :amount      => params[:amount],
 						    :description => 'Belize Beauty',
 						    :currency    => 'gbp'
 						  )
