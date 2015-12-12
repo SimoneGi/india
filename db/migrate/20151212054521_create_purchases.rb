0aca4c53ce094ca4bc75b820bb3fe2f58bc7bdb7
Simone <Simone.v.gill@wpfentongroup.co.uk>
2015-12-12 11:19:58 +0530

    created local db to store data from user day14

---
+++ db/migrate/20151212054521_create_purchases.rb
@@ -0,0 +1,17 @@
+class CreatePurchases < ActiveRecord::Migration
+  def change
+    create_table :purchases do |t|
+     t.string :email
+     t.integer :amount
+     t.string :description
+     t.string :currency
+     t.string :customer_id
+     t.string :card
+     t.integer :product_id
+     t.timestamps
+    end
+  end
+end
+
+# Creation of a server to store purchaes made on the site. We are labelling information that we need to store
+#This will ensure that this information can be used for data analysis. Think salesforce IBM and HP ENTERPRISE
